dda0a2739f9d3b90ff87a4b0c7e536d36569cb09
dflynn-volpe <daniel.flynn@dot.gov>
2022-05-19 13:28:42 -0400

    URL fixes, workflow update
    - Replaces previous `concept_primer` with new comprehensive user guide, including all prior concept primer content. Update to the github action yml makes this change propagate to visioneval.org.
    - Adds small script for book rendering
    - Updates URLs

---
+++ user_guide/Render_the_book.R
@@ -0,0 +1,14 @@
+# Rendering the VisionEval user's guide
+# launch from user_guide.Rproj
+
+depends = c('bookdown', 'downlit')
+
+for(i in depends){
+  if(!i %in% (.packages(all.available=T))){
+    install.packages(i, dependencies =TRUE)
+  }
+}
+
+bookdown::render_book()
+
+shell.exec(file.path(dirname(getwd()), 'docs/index.html'))
